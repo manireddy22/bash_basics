74fadf589451964dd5d18f15289d7d655e8cefbd
manipal <manipalreddyt6@gmail.com>
2022-07-02 13:23:21 +0000

    adding fourth number

--- 02-add_nums.sh
+++ 02-add_nums.sh
@@ -6,8 +6,10 @@ echo "Enter your second number"
 read second
 echo "Enter your third number"
 read third
+echo "Enter your fourth number"
+read fourth
 
-sum=$(( first+second+third ))
+sum=$(( first+second+third+fourth ))
 
 echo "The sum is $sum"
 
